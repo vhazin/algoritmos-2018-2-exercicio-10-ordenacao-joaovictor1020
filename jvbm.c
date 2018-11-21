b77f9829ca22d37f7b9f82f0e11e3a65c9b58290
João Victor Montarroios <39501506+joaovictor1020@users.noreply.github.com>
2018-11-21 15:58:24 -0200

    Update jvbm.c

--- jvbm.c
+++ jvbm.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(){
+    
+    int quant, name, comportados, ncomportados;
+    char vetor[name], aux[21], good;
+    scanf("%i", &quant);
+    while (quant--){
+        scanf("%i", &name);
+        if(good == '+'){
+            comportados++;
+        }
+        else{
+            ncomportados++;
+        }
+    }
+    for(int i = 0; i < quant; i++){
+        for(int j = i + 1; j < quant; j++){
+            if(strcmp(vetor[j], vetor[i]) == -1){
+                strcpy(aux, vetor[i]);
+                strcpy(vetor[i], vetor[j]);
+                strcpy(vetor[j], aux);
+            }
+        }
+    }
+    printf("Se comportaram: %i | Não se comportaram: %i\n", comportados, ncomportados);
+
+    return 0;
+}
